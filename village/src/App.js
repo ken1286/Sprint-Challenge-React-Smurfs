d1fed5a52d7a3de2bece4b174ed83e0633aeb7d0
ken1286 <kindle1286@gmail.com>
2019-05-10 08:15:13 -0700

    Routes made

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import axios from 'axios';
+import { Route, NavLink } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -31,14 +33,30 @@ class App extends Component {
       })
       .catch(err => {console.log(err)})
   }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm addSmurf={this.addSmurf}/>
         <Smurfs smurfs={this.state.smurfs} />
+
+        <Route 
+          exact
+          path='/'
+          render={props => (
+            <Smurfs {...props} smurfs={this.state.smurfs} />
+          )}
+        />
+        <Route
+          path='/smurf-form'
+          render={props => (
+            <SmurfForm {...props} addSmurf={this.addSmurf} />
+          )}
+        />
       </div>
     );
   }
